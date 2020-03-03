cbcb162fe80aac127f6efb60c3c03f242665b494
qburnsFB <quinton@giddy.io>
2020-03-03 11:08:52 -0500

    fix btn

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -11,7 +11,7 @@ class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      joinBtn: false,
+      joinBtn: true,
       channel: 'chibo',
       baseMode: 'avc',
       transcode: 'interop',
